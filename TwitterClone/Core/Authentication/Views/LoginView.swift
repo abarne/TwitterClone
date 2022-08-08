1aa4bc59bf310ad5ea1ac74c45835dc2d78af7aa
abarne <celius377@gmail.com>
2022-08-08 18:19:36 -0400

    Login page UI done

--- TwitterClone/Core/Authentication/Views/LoginView.swift
+++ TwitterClone/Core/Authentication/Views/LoginView.swift
@@ -8,9 +8,86 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        //parent containter
+        VStack{
+            //header view
+            VStack(alignment: .leading){
+                HStack{Spacer()}
+                Text("Hello.")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                Text("Welcome Back")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+            }
+            .frame(height: 260)
+            .padding(.leading)
+            .background(Color(.systemBlue))
+            .foregroundColor(.white)
+            .clipShape(RoundedShape(corners: [.bottomRight]))
+            
+            VStack(spacing: 40){
+                //TextField("Email", text: $email)
+                CustomInputField(imageName: "envelope", placeholderText: "Email...", text: $email)
+                
+                //TextField("Password", text: $password)
+                CustomInputField(imageName: "lock", placeholderText: "Password...", text: $password)
+            }
+            .padding(.horizontal, 32)
+            .padding(.top, 44)
+            
+            HStack{
+                Spacer()
+                NavigationLink{
+                    Text("Reset password view")
+                } label: {
+                    Text("Forgot Password?")
+                        .font(.caption)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color(.systemBlue))
+                        .padding(.top)
+                        .padding(.trailing, 24)
+                }
+            }
+            Button {
+                print("sign in here")
+            } label: {
+                Text("Sign in")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(width: 340, height: 50)
+                    .background(Color(.systemBlue))
+                    .clipShape(Capsule())
+                    .padding()
+            }
+            .shadow(color: .gray.opacity(0.5), radius: 10, x: 0, y: 5)
+
+            
+            Spacer()
+            
+            NavigationLink {
+                RegistrationView()
+                    .navigationBarHidden(true)
+            } label: {
+                HStack{
+                    Text("Don't have an account?")
+                        .font(.footnote)
+                    Text("Sign up")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                }
+            }
+            .padding(.bottom, 32)
+            
+        }
+        .ignoresSafeArea()
+        .foregroundColor(Color(.systemBlue))
+        .navigationBarHidden(true)
     }
+        
 }
 
 struct LoginView_Previews: PreviewProvider {
---
+++ TwitterClone/Core/Components/RoundedShape.swift
@@ -0,0 +1,18 @@
+//
+//  RoundedShape.swift
+//  TwitterClone
+//
+//  Created by admin on 8/8/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct RoundedShape: Shape {
+    var corners: UIRectCorner
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: 80, height: 80))
+        return Path(path.cgPath)
+    }
+}
